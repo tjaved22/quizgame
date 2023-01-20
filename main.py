36fc153d47276745e0bc54440e0e5721d1785a24
Talha Javed <talhajaveditdev@gmail.com>
2023-01-20 14:28:45 -0500

    Finished quiz game questions and program.  It now works.

--- main.py
+++ main.py
@@ -21,3 +21,20 @@ if answer.lower() == "graphics processing unit":
     score += 1
 else:
     print("Incorrect!")
+
+answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score / 4) * 100) + "%.")
