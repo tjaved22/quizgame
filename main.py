a56099a4cbbe9e2b4b558eb525a82621141c0c23
Talha Javed <talhajaveditdev@gmail.com>
2023-01-20 14:27:05 -0500

    Added more lines to game.

---
+++ main.py
@@ -0,0 +1,23 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :)")
+score = 0
+
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("Incorrect!")
